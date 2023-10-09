0a0a17a62be4f84b2711b7697ca8ed22ce97d2c0
Rosantii <rosantiii61@gmail.com>
2023-10-09 15:21:41 +0700

    Day1

---
+++ Day1/src/main/java/com/day1/Day1.java
@@ -0,0 +1,13 @@
+package com.day1;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        //Menampilkan data ke layar
+        System.out.println("Hello Word");
+        
+        //Menampilkan data lain
+        int angka=2;
+        System.out.println("Angka favorit saya adalah : "+angka);
+    }
+}
