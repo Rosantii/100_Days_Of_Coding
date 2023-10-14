52f89bc6f99fdea1778811579987c1e9796d9882
Rosantii <rosantiii61@gmail.com>
2023-10-14 12:21:25 +0700

    Day5

---
+++ Day5/src/day5/Day5.java
@@ -0,0 +1,21 @@
+
+package day5;
+
+
+public class Day5 {
+
+    public static void main(String[] args) {
+      
+        int panjang=3;
+        int alas=2;
+        int sisi=4;
+        
+        double hasil = 0.5*alas*panjang;
+        System.out.println("Luas : "+ hasil);
+        
+        double Hasil = sisi+sisi+sisi;
+        System.out.println("Keliling : "+Hasil);
+   
+    }
+    
+}
