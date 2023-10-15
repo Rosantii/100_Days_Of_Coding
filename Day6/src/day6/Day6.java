d3327baceb9c079a2b227a7055da5c4e20b6a5c3
Rosantii <rosantiii61@gmail.com>
2023-10-15 16:33:52 +0700

    Day6

---
+++ Day6/src/day6/Day6.java
@@ -0,0 +1,27 @@
+
+package day6;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+
+
+        // Tipe Data Non-Primitif (Reference)
+        Byte byteValue = 127;
+        Short  shortValue= 32000;
+        Long longValue = 1234567890L;
+        Float floatValue = 3.14f;
+        Double doubleValue = 2.71828;
+
+        System.out.println("Tipe Data Non-Primitif:");
+        System.out.println("Byte: " + byteValue);
+        System.out.println("Short: " + shortValue);
+        System.out.println("Long: " + longValue);
+        System.out.println("Float: " + floatValue);
+        System.out.println("Double: " + doubleValue);
+    }
+}
+
+    
+    
+
