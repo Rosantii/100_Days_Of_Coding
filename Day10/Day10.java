4d09a9f2c7ca043f276af1b071f76b693056ab68
Rosantii <rosantiii61@gmail.com>
2023-10-19 20:01:30 +0700

    Day10 operator aritmatika menghitung keliling persegi panjang

---
+++ Day10/Day10.java
@@ -0,0 +1,19 @@
+
+package day9;
+
+
+public class Day10 {
+    public static void main(String[] args) {
+        
+       //menghitung keliling persegi panjang
+    
+        double K,P,L;
+        
+        P=2.0;
+        L=4.0;
+        
+        K= (2*P) + (2*L);
+        
+        System.out.println(K);
+    }
+}
