658d528156dfa698c85dad5e65071b1d0098af95
Rosantii <rosantiii61@gmail.com>
2023-10-19 19:25:15 +0700

    Day10 keliling persegi panjang

--- Day9/src/day9/Day9.java
+++ Day9/src/day9/Day9.java
@@ -1,61 +1,52 @@
+#include <stdio.h>
+#include <graphics.h>
 
-package day9;
+void drawLineDDA(int x1, int y1, int x2, int y2) {
+    int dx, dy, steps, k;
+    float xIncrement, yIncrement, x, y;
 
-import java.util.Scanner;
+    // Hitung perubahan x dan y
+    dx = x2 - x1;
+    dy = y2 - y1;
 
-public class Day9 {
-
-    public static void main(String[] args) {
-        int a,b,hasil;
-        
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Input angka-1 : ");
-        a = sc.nextInt();
-        System.out.print("Input angka-2 : ");
-        b = sc.nextInt();
-        
-        //Penjumlahan
-        hasil = a+b;
-        System.out.println("Hasil : "+hasil);
-        
-        System.out.print("Input angka-1 : ");
-        a = sc.nextInt();
-        System.out.print("Input angka-2 : ");
-        b = sc.nextInt();
-        
-        //Pengurangan
-        hasil = a-b;
-        System.out.println("Hasil : "+hasil);
-        
-        System.out.print("Input angka-1 : ");
-        a = sc.nextInt();
-        System.out.print("Input angka-2 : ");
-        b = sc.nextInt();
-        
-        //Perkalian
-        hasil = a*b;
-        System.out.println("Hasil : "+hasil);
-        
-        System.out.print("Input angka-1 : ");
-        a = sc.nextInt();
-        System.out.print("Input angka-2 : ");
-        b = sc.nextInt();
-        
-        //Pembagian
-        hasil = a/b;
-        System.out.println("Hasil : "+hasil );
-        
-        System.out.print("Input angka-1 : ");
-        a = sc.nextInt();
-        System.out.print("Input angka-2 : ");
-        b = sc.nextInt();
-    
-            
-        //Sisa bagi
-        hasil = a%b;
-        System.out.println("Hasil : "+hasil);
-         
+    // Tentukan langkah maksimum
+    if (abs(dx) > abs(dy)) {
+        steps = abs(dx);
+    } else {
+        steps = abs(dy);
     }
+
+    // Hitung inkremen x dan y
+    xIncrement = (float)dx / (float)steps;
+    yIncrement = (float)dy / (float)steps;
+
+    x = x1;
+    y = y1;
+
+    // Gambar setiap titik pada garis
+    for (k = 0; k < steps; k++) {
+        putpixel(round(x), round(y), WHITE);
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "C:\\Dev-Cpp\\MinGW64\\lib\\libbgi.a");
+
+    int x1, y1, x2, y2;
     
+    printf("Masukkan koordinat titik awal (x1, y1): ");
+    scanf("%d %d", &x1, &y1);
+    
+    printf("Masukkan koordinat titik akhir (x2, y2): ");
+    scanf("%d %d", &x2, &y2);
+
+    drawLineDDA(x1, y1, x2, y2);
+
+    delay(5000);
+    closegraph();
+
+    return 0;
 }
