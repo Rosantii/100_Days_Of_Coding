eb6ed8c760a705dd0a609146f639228764d7e71a
Rosantii <rosantiii61@gmail.com>
2023-10-20 19:55:28 +0700

    Operator penugasan

---
+++ Day11/Day11.java
@@ -0,0 +1,34 @@
+
+package day9;
+
+public class Day11 {
+    public static void main(String[] args) {
+        
+        // operator penugasan
+       
+        //pengisian nilai
+        int b = 2;
+        int c = 5;
+        
+        //penambahan
+        b += c;
+        System.out.println("Penambahan : " +b);
+        
+        //pengurangan
+        b -= c;
+        System.out.println("Pengurangan : " +b);
+        
+        //perkalian
+        b *= c;
+        System.out.println("Perkalian : " +b);
+        
+        //pembagian
+        b /= c;
+        System.out.println("Pembagian : " +b);
+        
+        //sisa bagi
+        b %= c;
+        System.out.println("Sisa bagi : " +b);
+        
+    }
+}
