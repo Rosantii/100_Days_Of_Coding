068680ad464be599d2754187043841f98dd17c6a
Rosantii <rosantiii61@gmail.com>
2023-10-21 20:20:40 +0700

    Operator pembanding

---
+++ Day12/Day12.java
@@ -0,0 +1,37 @@
+
+package day9;
+
+public class Day12 {
+    public static void main(String[] args) {
+        
+        //Operator pembanding
+        int a=12;
+        int b=6;
+        boolean hasil;
+        
+        //Apakah a lebih besar dari b
+        hasil = a>b;
+        System.out.println(hasil);
+        
+        //Apakah a lebih kecil dari b
+        hasil = a<b;
+        System.out.println(hasil);
+        
+        //Apakah a lebih besar samadengan b
+        hasil = a>=b;
+        System.out.println(hasil);
+        
+        //Apakah a lebih kecil samadengan b
+        hasil = a<=b;
+        System.out.println(hasil);
+        
+        //Apakah a sama dengan b
+        hasil = a==b;
+        System.out.println(hasil);
+        
+        //Apakah a tidak samadengan b
+        hasil = a!=b;
+        System.out.println(hasil);
+        
+    }
+}
