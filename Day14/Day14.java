7223bc53c7aff2e5c68ef56d903b4404b7e88bd0
Rosantii <rosantiii61@gmail.com>
2023-10-23 20:26:18 +0700

    Operator ternary

---
+++ Day14/Day14.java
@@ -0,0 +1,19 @@
+
+package day9;
+
+public class Day14 {
+    public static void main(String[] args) {
+        
+        //operator ternary
+        
+        boolean suka = false;
+        String jawaban;
+        
+        //menggunakan operator ternary
+        jawaban = suka ? "iya" : "tidak";
+        
+        //menampilkan jawaban
+        System.out.println(jawaban);
+    }
+    
+}
