d331f0b38d91513b8bbd511cd64deb5b47784545
Rosantii <rosantiii61@gmail.com>
2023-10-24 18:37:27 +0700

    Array

---
+++ Day15/Day15.java
@@ -0,0 +1,13 @@
+
+package day9;
+
+public class Day15 {
+    public static void main(String[] args) {
+        
+        //membuat array
+        String nama[] = {"linda","santi","ayu"};
+        
+        //mengambil data array
+        System.out.println(nama[2]);
+    }
+}
