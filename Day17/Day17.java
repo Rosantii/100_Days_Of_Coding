d42c84b18938d23b7c5d6c7220fb8a327ad02301
Rosantii <rosantiii61@gmail.com>
2023-10-26 20:22:54 +0700

    Array list

---
+++ Day17/Day17.java
@@ -0,0 +1,23 @@
+
+package day9;
+import java.util.ArrayList;
+public class Day17 {
+    public static void main(String[] args) {
+        
+        //Array list
+        
+        //membuat objekvarray list
+        ArrayList kantongAjaib = new ArrayList();
+        
+        //mengisi kantong ajaib
+        kantongAjaib.add("senter pembesar");
+        kantongAjaib.add("tikus");
+        
+        //menampilkan isi kantong ajaib
+        System.out.println(kantongAjaib);
+        
+        //menampilkan banyak isi kantong ajaib
+        System.out.println("Kantong ajaib berisi "+kantongAjaib.size()+ "item");
+        }
+    }
+
