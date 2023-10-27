3a8d81c4069fe3346f661323c3b593e835894852
Rosantii <rosantiii61@gmail.com>
2023-10-27 07:36:12 +0700

    Array 2D

---
+++ Day18/Day18.java
@@ -0,0 +1,13 @@
+
+package day9;
+
+public class Day18 {
+    public static void main(String[] args) {
+        String [][] k = {{"lili","0823"},{"lala","0835"}};
+        
+        for (int i = 0; i < k.length; i++) {
+            System.out.println("Nama : "+k[i][0]);
+            System.out.println("Kontak : "+k[i][1]);
+        }
+    }
+}
