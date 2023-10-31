6293c22d5dc1dae185abad23452a8b160eb79519
Rosantii <rosantiii61@gmail.com>
2023-10-31 18:45:17 +0700

    Operator logika dalam percabangan

---
+++ Day22/Day22.java
@@ -0,0 +1,17 @@
+
+package day9;
+
+
+public class Day22 {
+    public static void main(String[] args) {
+        boolean SIM=false;
+        boolean STNK=true;
+        
+        //cek apakah dia akan ditilang
+        if (SIM == true && STNK == true) {
+            System.out.println("Tidak ditilang");
+        } else {
+            System.out.println("Anda ditilang");
+        }
+    }
+}
