b7591306f0afaccf7b672d28d310d19ce217c0c3
Rosantii <rosantiii61@gmail.com>
2023-11-02 18:05:39 +0700

    Perulangan For

---
+++ Day23/Day23.java
@@ -0,0 +1,12 @@
+
+package day9;
+
+public class Day23 {
+    public static void main(String[] args) {
+          //menampilkan bilangan ganjil saja 
+        for (int i = 1; i <= 20; i += 2 ) {
+            System.out.println(i+"");
+            
+        }
+    }
+}
