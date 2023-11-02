eb5e093edda4f0044ffcde1d3aef3e55a5f130f2
Rosantii <rosantiii61@gmail.com>
2023-11-02 18:06:53 +0700

    Perulangan For Each

---
+++ Day24/Day24.java
@@ -0,0 +1,16 @@
+
+package day9;
+
+public class Day24 {
+    public static void main(String[] args) {
+        //membuat array 
+        int angka[] = {1,2,3};
+        
+        //menggunakan perulangan for each untuk menampilkan angka
+        for (int x : angka) {
+            System.out.println(x+"");
+            
+        }
+        
+    }
+}
