666f69b4e2f4827dcf548cb8d8983a424a1638dd
Rosantii <rosantiii61@gmail.com>
2023-11-04 19:45:35 +0700

    Increment dan decrement di perulangan for

---
+++ Day26/Day26.java
@@ -0,0 +1,31 @@
+package day9;
+
+public class Day26 {
+    public static void main(String[] args) {
+        
+        // Increment dengan +=
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println(i);
+            
+        }
+        //Decrement dengan -=
+        for (int i = 10; i > 0; i -= 2) {
+            System.out.println(i);
+            
+        }
+        // Increment dengan ++ 
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            
+            
+        }
+        //Decrement dengan --
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+            
+        }
+}
+
+    }
+    
+
