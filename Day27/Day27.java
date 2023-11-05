5fb867736faf638b25126045bb561e80a0a1cd02
Rosantii <rosantiii61@gmail.com>
2023-11-05 20:26:40 +0700

    Perulangan while dan do while

---
+++ Day27/Day27.java
@@ -0,0 +1,21 @@
+
+package day9;
+
+
+public class Day27 {
+    public static void main(String[] args) {
+            //perulangan while
+            int a =1;
+            while(a<=5){
+                System.out.println(a);
+                a++;
+            }
+            
+            //perulangan do while
+            int b = 1;
+            do {
+                System.out.println(b);
+                b++;
+            } while (b <= 5);
+    }
+}
