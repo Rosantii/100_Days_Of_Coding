44414fb24ef61624c01e4bcca6f9944b6a8d4e3b
Rosantii <rosantiii61@gmail.com>
2023-11-09 20:35:00 +0700

    Update variabel

---
+++ Day30/Day30.java
@@ -0,0 +1,15 @@
+package day9;
+
+public class Day30 {
+    public static void main(String[] args) {
+        //mendeklarasikan dan menginisialisasi variabel
+        int angka = 2;
+        //menampilkan nilai awal variabel
+        System.out.println("Nilai awal variabel :"+angka);
+        //mengupdate nilai variabel
+        angka =5;
+        //menampilkan nilai variabel setelah di update
+        System.out.println("Nilai variabel setelah di update :"+angka);
+    }
+    
+}
