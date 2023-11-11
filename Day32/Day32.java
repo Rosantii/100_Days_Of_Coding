cb2b60e131f4db8f47f50451629fefe891a6d66d
Rosantii <rosantiii61@gmail.com>
2023-11-11 08:17:17 +0700

    Infinite while

---
+++ Day32/Day32.java
@@ -0,0 +1,9 @@
+package day9;
+
+public class Day32 {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Ini adalah infinite Loop menggunakan while");
+        }
+    }
+}
