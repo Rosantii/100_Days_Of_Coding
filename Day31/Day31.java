66446bd181054485b468dd6577498e62f92afbb6
Rosantii <rosantiii61@gmail.com>
2023-11-11 08:16:34 +0700

    Infinite for

---
+++ Day31/Day31.java
@@ -0,0 +1,13 @@
+package day9;
+
+public class Day31 {
+    public static void main(String[] args) {
+       for(;;){
+           System.out.println("Ini adalah infinite Loop menggunakan for");   
+        }
+       
+       }
+    }
+    
+    
+
