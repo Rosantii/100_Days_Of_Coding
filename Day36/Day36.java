99e4702ec9e19d60fa13dc29d7fe45fcc802d0e6
Rosantii <rosantiii61@gmail.com>
2023-11-15 21:34:30 +0700

    Deklarasi array bertipe primitif double

---
+++ Day36/Day36.java
@@ -0,0 +1,22 @@
+package day9;
+
+public class Day36 {
+    public static void main(String[] args) {
+        double[] data = new double[3];
+        
+        data[0] = 1.2;
+        data[1] = 1.3;
+        data[2] = 1.4;
+        
+        System.out.println("Nilai array data: ");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+            
+            
+        }
+ {
+            
+        }
+    }
+    
+}
