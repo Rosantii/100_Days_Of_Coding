e0013faa4229e037033c1dcc9d897d78887fdfe7
Rosantii <rosantiii61@gmail.com>
2023-11-15 20:05:10 +0700

    Deklarasi array bertipe primitif

---
+++ Day34/Day34.java
@@ -0,0 +1,14 @@
+package day9;
+
+public class Day34 {
+    int[] angka; //Deklarasi array bertipe int
+    double[] data; //dekalarasi array bertipe double
+    char[] karakter; //dekalarasi array bertipe char
+    boolean[] kondisi; //deklarasi array bertipe boolean
+    byte[] byteData; //deklarasi array bertipe byte
+    float[] floatData; //deklarasi array bertipe float
+    short[] shortdata; //deklarasi array bertipe short
+    long[] longData; //deklarasi array bertipe long
+    
+
+}
