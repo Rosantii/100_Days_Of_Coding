95271d4ef0b15756461988bacaa10d398920bc3b
Rosantii <rosantiii61@gmail.com>
2023-11-15 21:36:04 +0700

    Deklarasi array bertipe primitif int

---
+++ Day37/Day37.java
@@ -0,0 +1,19 @@
+package day9;
+
+public class Day37 {
+     public static void main(String[] args) {
+        int[] b= new int[4];
+        
+        b[0] = 1;
+        b[1] = 2;
+        b[2] = 3;
+        b[3] = 4;
+        
+        System.out.println("Nilai array b:");
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+            
+        }
+    }
+   
+}
