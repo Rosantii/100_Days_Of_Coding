db13ec1e51fd172737424fdbf610d17069ef6569
Rosantii <rosantiii61@gmail.com>
2023-11-15 21:33:16 +0700

    Deklarasi array bertipe primitif char

---
+++ Day35/Day35.java
@@ -0,0 +1,17 @@
+package day9;
+
+public class Day35 {
+    public static void main(String[] args) {
+        char[] c = new char[2];
+        
+        c[0] = 'A';
+        c[1] = 'B';
+        
+        System.out.println("Nilai array c:");
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+            
+        }
+    }
+      
+}
