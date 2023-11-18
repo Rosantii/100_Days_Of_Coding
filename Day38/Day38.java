2b0a9488cc2dbcd23b8dfc0648d0404e3762efa6
Rosantii <rosantiii61@gmail.com>
2023-11-18 18:27:53 +0700

    Update variabel ; ' >

---
+++ Day38/Day38.java
@@ -0,0 +1,18 @@
+package day9;
+
+public class Day38 {
+    public static void main(String[] args) {
+        //mendeklarasi dan menginisialisasi variabel
+        String namaDepan = "Rosa";
+        String namaBelakang ="Linda";
+        
+        //Update variabel
+        namaDepan = "Muh";
+        namaBelakang = "Cahyo";
+        
+        
+        System.out.println("Nama depan : "+namaDepan);
+        System.out.println("Nama belakang :"+namaBelakang);
+    }
+
+}
